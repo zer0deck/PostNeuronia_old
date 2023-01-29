bee7c755312041d21bd150bfdbaa7183b465ff16
zer0deck <zer0deck@icloud.com>
2023-01-30 00:27:17 +0300

    fix image generation

--- caption_generator/train_model.py
+++ caption_generator/train_model.py
@@ -3,10 +3,10 @@ from prepare_dataset import preprocess
 from caption_generator import PostNeuronia
 
 def train_model(epochs = 10):
-    dataset, tokenizer, image_features_extract_model, num_steps = preprocess()
-    model = PostNeuronia(tokenizer, image_features_extract_model, num_steps)
+    dataset, tokenizer, num_steps = preprocess()
+    model = PostNeuronia(tokenizer, num_steps)
     model.train(dataset, num_epoch=epochs)
     model.test()
 
-# if __name__ == '__main__':
-#     train_model(epochs=10)
+if __name__ == '__main__':
+    train_model(epochs=6)
