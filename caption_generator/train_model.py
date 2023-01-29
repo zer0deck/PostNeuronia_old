933652716161a782a6a6c79b37c6649321ee9dc5
zer0deck <zer0deck@icloud.com>
2023-01-29 03:18:09 +0300

    update within new model

--- caption_generator/train_model.py
+++ caption_generator/train_model.py
@@ -1,26 +1,12 @@
 # pylint:disable=[all]
-import caption_generator
-import tensorflow as tf
+from prepare_dataset import preprocess
+from caption_generator import PostNeuronia
 
-def train_model(weight = None, batch_size=32, epochs = 10):
+def train_model(epochs = 10):
+    dataset, tokenizer, image_features_extract_model, num_steps = preprocess()
+    model = PostNeuronia(tokenizer, image_features_extract_model, num_steps)
+    model.train(dataset, num_epoch=epochs)
+    model.test()
 
-    cg = caption_generator.CaptionGenerator()
-    model = cg.create_model()
-
-    if weight != None:
-        model.load_weights(weight)
-
-    counter = 0
-    file_name = 'weights-improvement-{epoch:02d}.hdf5'
-    # checkpoint = tf.keras.callbacks.ModelCheckpoint(file_name, monitor='loss', verbose=1, save_best_only=True, mode='min')
-    # callbacks_list = [checkpoint]
-    model.fit_generator(cg.data_generator(batch_size=batch_size), steps_per_epoch=cg.total_samples/batch_size, epochs=epochs, verbose=2)
-    try:
-        model.save('Models/WholeModel.h5', overwrite=True)
-        model.save_weights('Models/Weights.h5',overwrite=True)
-    except:
-        print ("Error in saving model.")
-    print ("Training complete...\n")
-
-if __name__ == '__main__':
-    train_model(epochs=50)
+# if __name__ == '__main__':
+#     train_model(epochs=10)
