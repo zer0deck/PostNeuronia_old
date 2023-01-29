c76669fed07eece1c4d8e876a51b975ac29c72ae
zer0deck <zer0deck@icloud.com>
2023-01-29 03:17:22 +0300

    new attention model

--- caption_generator/caption_generator.py
+++ caption_generator/caption_generator.py
@@ -1,144 +1,244 @@
-# pylint:disable=[all]None
-from vgg16 import VGG16
-import numpy as np
-import pandas as pd
+# pylint:disable=[all]
 import tensorflow as tf
-import _pickle as pickle
+import collections
+import random
+import numpy as np
+import json
+import time
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+from prepare_dataset import load_image
+from PIL import Image
 
-EMBEDDING_DIM = 128
+# HYPERPARAMS
 
+FP = 'E:/Projects/PostNeuronia/'
+MAX_LENGTH = 50
+VOCAB_SIZE = 5000
+BATCH_SIZE = 64
+BUFFER_SIZE = 1000
+embedding_dim = 256
+units = 512
+features_shape = 2048
+attention_features_shape = 64
 
-class CaptionGenerator():
+####################################
 
-    def __init__(self):
-        self.max_cap_len = None
-        self.vocab_size = None
-        self.index_word = None
-        self.word_index = None
-        self.total_samples = None
-        self.encoded_images = pickle.load( open( "encoded_images.p", "rb" ) )
-        self.variable_initializer()
+class BahdanauAttention(tf.keras.Model):
+    def __init__(self, units):
+        super(BahdanauAttention, self).__init__()
+        self.W1 = tf.keras.layers.Dense(units)
+        self.W2 = tf.keras.layers.Dense(units)
+        self.V = tf.keras.layers.Dense(1)
 
-    def variable_initializer(self):
-        df = pd.read_csv('Flickr8k_text/flickr_8k_train_dataset.txt', delimiter='\t')
-        nb_samples = df.shape[0]
-        iterator = df.iterrows()
-        caps = []
-        for i in range(nb_samples):
-            x = next(iterator)
-            caps.append(x[1][1])
+    def call(self, features, hidden):
+        hidden_with_time_axis = tf.expand_dims(hidden, 1)
+        attention_hidden_layer = (tf.nn.tanh(self.W1(features) +
+                                            self.W2(hidden_with_time_axis)))
 
-        self.total_samples=0
-        for text in caps:
-            self.total_samples+=len(text.split())-1
-        print ("Total samples : "+str(self.total_samples))
+        score = self.V(attention_hidden_layer)
+        attention_weights = tf.nn.softmax(score, axis=1)
+        context_vector = attention_weights * features
+        context_vector = tf.reduce_sum(context_vector, axis=1)
+
+        return context_vector, attention_weights
+
+class CNN_Encoder(tf.keras.Model):
+    def __init__(self, embedding_dim):
+        super(CNN_Encoder, self).__init__()
+        self.fc = tf.keras.layers.Dense(embedding_dim)
+
+    def call(self, x):
+        x = self.fc(x)
+        x = tf.nn.relu(x)
+        return x
+
+class RNN_Decoder(tf.keras.Model):
+    def __init__(self, embedding_dim, units, vocab_size):
+        super(RNN_Decoder, self).__init__()
+        self.units = units
+
+        self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+        self.gru = tf.keras.layers.GRU(self.units,
+                                    return_sequences=True,
+                                    return_state=True,
+                                    recurrent_initializer='glorot_uniform')
+        self.fc1 = tf.keras.layers.Dense(self.units)
+        self.fc2 = tf.keras.layers.Dense(vocab_size)
+        self.attention = BahdanauAttention(self.units)
+
+    def call(self, x, features, hidden):
+        context_vector, attention_weights = self.attention(features, hidden)
+        x = self.embedding(x)
+        x = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
+        output, state = self.gru(x)
+        x = self.fc1(output)
+        x = tf.reshape(x, (-1, x.shape[2]))
+        x = self.fc2(x)
+
+        return x, state, attention_weights
+
+    def reset_state(self, batch_size):
+        return tf.zeros((batch_size, self.units))
+
+class PostNeuronia():
+    def __init__(self, tokenizer: tf.keras.layers.TextVectorization, image_features_extract_model, num_steps: int) -> None:
+        self.encoder = CNN_Encoder(embedding_dim)
+        self.decoder = RNN_Decoder(embedding_dim, units, tokenizer.vocabulary_size())
+        self.optimizer = tf.keras.optimizers.Adam()
+        self.image_features_extract_model = image_features_extract_model
+        self.loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
+            from_logits=True, reduction='none')
+        checkpoint_path = f"{FP}models/checkpoints/train"
+        self.ckpt = tf.train.Checkpoint(encoder=self.encoder,
+                                decoder=self.decoder,
+                                optimizer=self.optimizer)
+        self.ckpt_manager = tf.train.CheckpointManager(self.ckpt, checkpoint_path, max_to_keep=5)
+        self.start_epoch = 0
+        if self.ckpt_manager.latest_checkpoint:
+            self.start_epoch = int(self.ckpt_manager.latest_checkpoint.split('-')[-1])
         
-        words = [txt.split() for txt in caps]
-        unique = []
-        for word in words:
-            unique.extend(word)
+        self.ckpt.restore(self.ckpt_manager.latest_checkpoint)
+        self.loss_plot = []
+        self.word_to_index = tf.keras.layers.StringLookup(
+            mask_token="",
+            vocabulary=tokenizer.get_vocabulary()
+        )
+        self.index_to_word = tf.keras.layers.StringLookup(
+            mask_token="",
+            vocabulary=tokenizer.get_vocabulary(),
+            invert=True
+        )
+        self.num_steps = num_steps
+    
+    def loss_function(self, real, pred):
+        mask = tf.math.logical_not(tf.math.equal(real, 0))
+        loss_ = self.loss_object(real, pred)
 
-        unique = list(set(unique))
-        self.vocab_size = len(unique)
-        self.word_index = {}
-        self.index_word = {}
-        for i, word in enumerate(unique):
-            self.word_index[word]=i
-            self.index_word[i]=word
+        mask = tf.cast(mask, dtype=loss_.dtype)
+        loss_ *= mask
 
-        max_len = 0
-        for caption in caps:
-            if(len(caption.split()) > max_len):
-                max_len = len(caption.split())
-        self.max_cap_len = max_len
-        print ("Vocabulary size: "+str(self.vocab_size))
-        print ("Maximum caption length: "+str(self.max_cap_len))
-        print ("Variables initialization done!")
+        return tf.reduce_mean(loss_)
 
+    @tf.function
+    def train_step(self, img_tensor, target):
+        loss = 0
 
-    def data_generator(self, batch_size = 32):
-        partial_caps = []
-        next_words = []
-        images = []
-        print ("Generating data...")
-        gen_count = 0
-        df = pd.read_csv('Flickr8k_text/flickr_8k_train_dataset.txt', delimiter='\t')
-        nb_samples = df.shape[0]
-        iterator = df.iterrows()
-        caps = []
-        imgs = []
-        for i in range(nb_samples):
-            x = next(iterator)
-            caps.append(x[1][1])
-            imgs.append(x[1][0])
+        # initializing the hidden state for each batch
+        # because the captions are not related from image to image
+        hidden = self.decoder.reset_state(batch_size=target.shape[0])
 
+        dec_input = tf.expand_dims([self.word_to_index('<start>')] * target.shape[0], 1)
 
-        total_count = 0
-        while 1:
-            image_counter = -1
-            for text in caps:
-                image_counter+=1
-                current_image = self.encoded_images[imgs[image_counter]]
-                for i in range(len(text.split())-1):
-                    total_count+=1
-                    partial = [self.word_index[txt] for txt in text.split()[:i+1]]
-                    partial_caps.append(partial)
-                    next_val = np.zeros(self.vocab_size)
-                    next_val[self.word_index[text.split()[i+1]]] = 1
-                    next_words.append(next_val)
-                    images.append(current_image)
+        with tf.GradientTape() as tape:
+            features = self.encoder(img_tensor)
 
-                    if total_count>=batch_size:
-                        next_words = np.asarray(next_words)
-                        images = np.asarray(images)
-                        partial_caps = tf.keras.preprocessing.sequence.pad_sequences(partial_caps, maxlen=self.max_cap_len, padding='post')
-                        total_count = 0
-                        gen_count+=1
-                        print ("yielding count: "+str(gen_count))
-                        print(len(images[0]), len(partial_caps[0]))
-                        yield [[images, partial_caps], next_words]
-                        partial_caps = []
-                        next_words = []
-                        images = []
-        
-    def load_image(self, path):
-        img = tf.keras.preprocessing.image.load_img(path, target_size=(224,224))
-        x = tf.keras.preprocessing.image.img_to_array(img)
-        return np.asarray(x)
+            for i in range(1, target.shape[1]):
+                # passing the features through the decoder
+                predictions, hidden, _ = self.decoder(dec_input, features, hidden)
 
+                loss += self.loss_function(target[:, i], predictions)
 
-    def create_model(self, ret_model = False):
-        base_model = VGG16(weights='imagenet', include_top=False, input_shape = (224, 224, 3))
-        base_model.trainable=False
-        image_model = tf.keras.Sequential(name='Convolutional')
-        image_model.add(base_model)
-        image_model.add(tf.keras.layers.Flatten())
-        image_model.add(tf.keras.layers.Dense(EMBEDDING_DIM, input_dim = 4096, activation='relu'))
-        image_model.add(tf.keras.layers.RepeatVector(self.max_cap_len))
+                # using teacher forcing
+                dec_input = tf.expand_dims(target[:, i], 1)
 
-        lang_model = tf.keras.Sequential(name='RNN')
-        lang_model.add(tf.keras.layers.Embedding(self.vocab_size, 256, input_length=self.max_cap_len))
-        lang_model.add(tf.keras.layers.LSTM(256,return_sequences=True))
-        lang_model.add(tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(EMBEDDING_DIM)))
+        total_loss = (loss / int(target.shape[1]))
 
-        comb_out = tf.keras.layers.add([image_model.output, lang_model.output])
-        # THANKS FOR https://stackoverflow.com/a/54104873
-        # print(f'{comb_out}')
-        input1 = tf.keras.Input(shape=(None, 4096), name='e1_inp')
-        input2 = tf.keras.Input(shape=(None, self.vocab_size), name='dec1_inp')
-        model = tf.keras.Sequential(name='Combined')
-        model.add(tf.keras.layers.LSTM(1000,return_sequences=False))
-        model.add(tf.keras.layers.Dense(self.vocab_size))
-        model.add(tf.keras.layers.Activation('softmax'))
-        f_model = tf.keras.Model(inputs = [base_model.input, lang_model.input], outputs = model(comb_out), name='final')
-        # f_model = model
-        print ("Model created!")
+        trainable_variables = self.encoder.trainable_variables + self.decoder.trainable_variables
 
-        if(ret_model==True):
-            return f_model
+        gradients = tape.gradient(loss, trainable_variables)
 
-        f_model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
-        return f_model
+        self.optimizer.apply_gradients(zip(gradients, trainable_variables))
 
-    def get_word(self,index):
-        return self.index_word[index]
+        return loss, total_loss
+    
+    def train(self, dataset:tf.data.Dataset, num_epoch = 20):
+        for epoch in range(self.start_epoch, num_epoch):
+            start = time.time()
+            total_loss = 0
+
+            for (batch, (img_tensor, target)) in enumerate(dataset):
+                batch_loss, t_loss = self.train_step(img_tensor, target)
+                total_loss += t_loss
+
+                if batch % 100 == 0:
+                    average_batch_loss = batch_loss.numpy()/int(target.shape[1])
+                    print(f'Epoch {epoch+1} Batch {batch} Loss {average_batch_loss:.4f}')
+
+            self.loss_plot.append(total_loss / self.num_steps)
+
+            if epoch % 5 == 0:
+                self.ckpt_manager.save()
+
+            print(f'Epoch {epoch+1} Loss {total_loss/self.num_steps:.6f}')
+            print(f'Time taken for 1 epoch {time.time()-start:.2f} sec\n')
+    
+    def print_plot(self):
+        plt.plot(self.loss_plot)
+        plt.xlabel('Epochs')
+        plt.ylabel('Loss')
+        plt.title('Loss Plot')
+        plt.show()
+
+    def evaluate(self, image):
+        attention_plot = np.zeros((MAX_LENGTH, attention_features_shape))
+
+        hidden = self.decoder.reset_state(batch_size=1)
+
+        temp_input = tf.expand_dims(load_image(image)[0], 0)
+        img_tensor_val = self.image_features_extract_model(temp_input)
+        img_tensor_val = tf.reshape(img_tensor_val, (img_tensor_val.shape[0],
+                                                    -1,
+                                                    img_tensor_val.shape[3]))
+
+        features = self.encoder(img_tensor_val)
+
+        dec_input = tf.expand_dims([self.word_to_index('<start>')], 0)
+        result = []
+
+        for i in range(MAX_LENGTH):
+            predictions, hidden, attention_weights = self.decoder(dec_input,
+                                                            features,
+                                                            hidden)
+
+            attention_plot[i] = tf.reshape(attention_weights, (-1, )).numpy()
+
+            predicted_id = tf.random.categorical(predictions, 1)[0][0].numpy()
+            predicted_word = tf.compat.as_text(self.index_to_word(predicted_id).numpy())
+            result.append(predicted_word)
+
+            if predicted_word == '<end>':
+                return result, attention_plot
+
+            dec_input = tf.expand_dims([predicted_id], 0)
+
+        attention_plot = attention_plot[:len(result), :]
+        return result, attention_plot
+
+    def plot_attention(self, image, result, attention_plot):
+        temp_image = np.array(Image.open(image))
+
+        fig = plt.figure(figsize=(10, 10))
+
+        len_result = len(result)
+        for i in range(len_result):
+            temp_att = np.resize(attention_plot[i], (8, 8))
+            grid_size = max(int(np.ceil(len_result/2)), 2)
+            ax = fig.add_subplot(grid_size, grid_size, i+1)
+            ax.set_title(result[i])
+            img = ax.imshow(temp_image)
+            ax.imshow(temp_att, cmap='gray', alpha=0.6, extent=img.get_extent())
+
+        plt.tight_layout()
+        plt.show()
+
+    def test(self):
+        image_url = 'https://tensorflow.org/images/surf.jpg'
+        image_extension = image_url[-4:]
+        image_path = tf.keras.utils.get_file('image'+image_extension, origin=image_url)
+
+        result, attention_plot = self.evaluate(image_path)
+        print('Prediction Caption:', ' '.join(result))
+        self.plot_attention(image_path, result, attention_plot)
+        # opening the image
+        Image.open(image_path)
